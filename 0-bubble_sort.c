d574a8dfdb0427e8f540e88ad017e06708965502
Amele06 <kelvina0306@gmail.com>
2023-12-22 10:18:17 -0800

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,27 +9,23 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, temp;
-	int swap;
+	size_t i, j;
+	int a;
 
 	for (i = 0 ; i < size - 1 ; i++)
 	{
-		swap = 0;
 
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				temp = array[j];
+				a = array[j];
 				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				swap = 1;
+				array[j + 1] = a;
 
 				print_array(array, size);
 			}
 		}
-		if (swap == 0)
-			break;
 	}
 }
 
