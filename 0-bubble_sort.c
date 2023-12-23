3234cdc66fad9ff310bf3d54689fa4d7d7f977af
Amele06 <kelvina0306@gmail.com>
2023-12-23 11:54:36 -0800

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,12 +9,14 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t j, i;
 	int a;
 
-	for (i = 0 ; i < size - 1 ; i++)
-	{
 
+	if (array == NULL || size == 0)
+		return;
+	for (i = 0; i < size; i++)
+	{
 		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -22,10 +24,8 @@ void bubble_sort(int *array, size_t size)
 				a = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = a;
-
 				print_array(array, size);
 			}
 		}
 	}
 }
-
